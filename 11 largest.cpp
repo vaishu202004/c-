5250448c77de6904e21141f2295789c504546008
vaishu202004 <124141580+vaishu202004@users.noreply.github.com>
2024-02-08 08:36:56 +0530

    Create 11 largest.cpp

---
+++ 11 largest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, num3;
+    cout << "Enter three integers: ";
+    cin >> num1 >> num2 >> num3;
+
+    int largest = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+
+    cout << "Largest number: " << largest << endl;
+
+    return 0;
+}
