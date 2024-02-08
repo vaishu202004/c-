a934feef2c382702f2eea01beae9e4611f791263
vaishu202004 <124141580+vaishu202004@users.noreply.github.com>
2024-02-08 08:36:10 +0530

    Create 10 swap.cpp

---
+++ 10 swap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+
+    cout << "Before swapping: num1 = " << num1 << ", num2 = " << num2 << endl;
+
+    num1 = num1 ^ num2;
+    num2 = num1 ^ num2;
+    num1 = num1 ^ num2;
+
+    cout << "After swapping: num1 = " << num1 << ", num2 = " << num2 << endl;
+
+    return 0;
+}
