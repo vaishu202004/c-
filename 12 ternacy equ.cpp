b7f3b5839550d551761642ae6b6f05db72e95e2a
vaishu202004 <124141580+vaishu202004@users.noreply.github.com>
2024-02-08 08:37:58 +0530

    Create 12 ternacy equ.cpp

---
+++ 12 ternacy equ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+
+    string result = (num1 == num2) ? "Numbers are equal." : "Numbers are not equal.";
+
+    cout << result << endl;
+
+    return 0;
+}
